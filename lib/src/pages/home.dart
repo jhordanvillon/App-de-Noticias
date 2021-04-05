5d610cee749e98c291664468bab8b58e8fd7742c
jhordanvillon <jhvillon@hotmail.com>
2021-04-05 08:58:29 -0500

    cards

--- lib/src/pages/home.dart
+++ lib/src/pages/home.dart
@@ -36,7 +36,7 @@ class HomePage extends StatelessWidget {
     );
   }
   Widget _card(){
-    return Container(
+    return Container( //la forma del card
       margin: EdgeInsets.only(top: 25),
       padding:EdgeInsets.symmetric(horizontal:10),
       child:  Container(
@@ -45,11 +45,53 @@ class HomePage extends StatelessWidget {
                   borderRadius: BorderRadius.circular(15)
         ),
         padding: EdgeInsets.only(top: 20),
-        height: 200,
-        width: 200,
+        height: 350,
+       
+
+        child: Column( // contenido del card
+          children: [
+            ListTile( //encabezado
+              title: Text("Jhordan Villón"),
+              subtitle: Text("Publicado en Lima, Perú 28 de Marzo"),
+              leading: CircleAvatar(
+                child: Text("J"),
+              ),
+            ),
+            Container( //imagen
+              padding: EdgeInsets.symmetric(horizontal:10),
+                          child: Container(
+                height: 180,
+                width:double.infinity, //ocupada todo el espacio disponible
+                decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                 image: DecorationImage(
+                   image: AssetImage("assets/unnamed.jpg"),
+                   fit: BoxFit.fill,
+                   
+                 )
+               ),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal:15),
+              margin: EdgeInsets.only(top:10),
+              child: Text("OG resultó campeón del International 9, haciendo acreedor de 15 millones de dólares.",style: TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
+              ),)
+            ), //titulo de la noticia
+          ],
+        ),
       )
     );
 
-  }
+  } // fin de todo el card.
 
 }
+/*
+ child: Image(
+                  fit: BoxFit.fill,
+                  image:AssetImage("assets/unnamed.jpg"),
+                  
+                )
+*/
